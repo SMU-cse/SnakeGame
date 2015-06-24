a0a6b02000fbadc49b6079379677ae8ab9c55c29
Chase <hamrdapps@gmail.com>
2015-06-23 19:27:36 -0500

    Previous frame rate too fast, halved

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 //========================================================================
 int main( ){
 	std::cout << ofGetFrameRate() << std::endl;
+	ofSetFrameRate(30);
 	ofSetupOpenGL(600,600,OF_WINDOW);			// <-------- setup the GL context
 	ofSetVerticalSync(false);
 
