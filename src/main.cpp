3a668e693d9e9d24559b95973e24d3c599eed76e
Chase <hamrdapps@gmail.com>
2015-06-23 19:25:16 -0500

    main updated with faster frame rate

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include "ofMain.h"
 #include "ofApp.h"
-
+#include "LinkedList.h"
+#include "Node.h"
 //========================================================================
 int main( ){
+	std::cout << ofGetFrameRate() << std::endl;
 	ofSetupOpenGL(600,600,OF_WINDOW);			// <-------- setup the GL context
+	ofSetVerticalSync(false);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+	// pass in width and height too
 	ofRunApp(new ofApp());
 
 }
